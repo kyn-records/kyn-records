ac55299ffe940c2aaa958a7d480a3995184d4cc3
Shrihari Prakasam <Shrihari.Prakasam@genesys.com>
2024-11-08 23:36:43 +0530

    Change stuff.

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -1,13 +1,13 @@
-import { FaRecordVinyl } from "react-icons/fa";
 import { Button } from "../ui/button";
 import { Link } from "wouter";
+import KYNLogo from "@/assets/kyn-logo.png"
 
 export const Header = () => {
   return (
     <div className="bg-neutral-900 p-4 text-white flex justify-between items-center sticky top-0 left-0 z-[99999] shadow-lg">
       <Link href="/">
         <div className="flex gap-2 items-between opacity-75 items-center">
-          <FaRecordVinyl size={38} />
+          <img src={KYNLogo} className="h-16" alt="KYN Records Logo" />
           <div>
             <h1 className="font-bold">KYN Records</h1>
             <p className="opacity-60">This is description placeholder</p>
